e41feffd646261062dfdb554aed3c25e65bb0f0c
tonghao.yuan <tonghao.yuan@csdental.com>
2019-10-16 15:37:51 +0800

    feat:create

---
+++ time_measurement.cpp
@@ -0,0 +1,102 @@
+#include "time_measurement.h"
+#include "common/utility/path.h"
+
+#include <cassert>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <mutex>
+#include <sstream>
+#include <thread>
+
+#include <QDir>
+
+namespace TM {
+
+TMString CtimeToString(const time_t t) {
+  std::tm time_info;
+  GetLocalTime(t, time_info);
+
+  // %Y-%m-%d %H:%M:%S
+  std::put_time(&time_info, "F% %T");
+
+  std::ostringstream os;
+  os << std::put_time(&time_info, "F% %T");
+
+  return os.str();
+}
+
+void GetLocalTime(const time_t t, TMTimeStruct &tm) {
+#ifdef _WIN32
+  localtime_s(&tm, &t);
+#else
+  localtime_r(&tm, &t);
+#endif
+}
+
+TMTimePoint GetCurrentTime() {
+  return TMClock::now();
+}
+
+void PrintExecutionResult(const TMString &name, const TMTimePoint start_time) {
+  auto stop_time_point = GetCurrentTime();
+
+  std::tm time_info;
+  GetLocalTime(std::chrono::system_clock::to_time_t(stop_time_point), time_info);
+
+  //  std::cout << std::put_time(&time_info, "%x %X")  // date time
+  //            << " [" << name << "] "
+  //            << "execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(stop_time_point - start_time).count() <<
+  //            "ms\n";
+
+  static std::ofstream tm_log(
+    path::GetAppDataDir() + QDir::separator().toLatin1() + "log" + QDir::separator().toLatin1() + "time_measurement.log",
+    std::ofstream::app);
+  static std::mutex    log_mutex;
+  assert(tm_log.is_open());
+
+  {
+    std::lock_guard<std::mutex> guard(log_mutex);
+    tm_log << std::put_time(&time_info, "%Y-%m-%d %H:%M:%S")  // date time
+           << " [\"" << name << "\"] "
+           << "execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(stop_time_point - start_time).count() << "ms\n";
+    tm_log.flush();
+  }
+}
+
+TMString GetComplexName(TMString &&original_name) {
+  std::ostringstream os;
+  os << std::this_thread::get_id();
+  return original_name + "@" + os.str();
+}
+
+BlockMeasurement::BlockMeasurement(TMString &&name) {
+  name_   = GetComplexName(std::move(name));
+  start_ = GetCurrentTime();
+}
+
+BlockMeasurement::~BlockMeasurement() {
+  PrintExecutionResult(name_, start_);
+}
+
+GeneralTimeMeasurement &GeneralTimeMeasurement::Instance() {
+  static GeneralTimeMeasurement instance;
+  return instance;
+}
+
+void GeneralTimeMeasurement::Start(std::string &&name) {
+  // insert start time
+  const auto it = measurement_map_.insert(std::make_pair(GetComplexName(std::move(name)), GetCurrentTime()));
+  assert(it.second && "name should be unique");
+}
+
+void GeneralTimeMeasurement::Stop(std::string &&name) {
+  auto it = measurement_map_.find(GetComplexName(std::move(name)));
+  assert(it != measurement_map_.end() && "you should call Start with the same name before call Stop");
+  PrintExecutionResult(it->first, it->second);
+
+  measurement_map_.erase(it);
+}
+
+}  // namespace TM
---
+++ time_measurement.h
@@ -0,0 +1,153 @@
+/**
+ * @file time_measurement.h
+ * @author tonghao.yuan (tonghao.yuan@csdental.com)
+ * @brief functions for measuring execute time of a snippet of code
+ * @version 0.1
+ * @date 2019-09-19
+ *
+ * @copyright Copyright (c) 2019 Carestream Dental,LLC. All Rights Reserved
+ *
+ */
+
+#pragma once
+
+#include <chrono>  // for precise timer
+#include <string>
+#include <unordered_map>
+
+#ifdef NDEBUG
+#  define EXPR_MEASURE(expression)
+#  define BLOCK_MEASURE
+#  define MEASURE_START(name)
+#  define MEASURE_STOP(name)
+#else
+#  include <sstream>  // EXPR_MEASURE
+#  define BLOCK_MEASURE TM::BlockMeasurement measure(__FUNCTION__)
+#  define MEASURE_START(name) TM::GeneralTimeMeasurement::Instance().Start(name)
+#  define MEASURE_STOP(name) TM::GeneralTimeMeasurement::Instance().Stop(name)
+
+/// measure execution time of one exprssion, like a function call
+/// @code{.cpp}
+/// EXPR_MEASURE(func()) // measure the execution time of func
+/// @endcode
+#  define EXPR_MEASURE(expression)                             \
+    {                                                          \
+      std::stringstream ss;                                    \
+      ss << __FILE__ << ":" << __LINE__ << " " << #expression; \
+      MEASURE_START(ss.str());                                 \
+      expression;                                              \
+      MEASURE_STOP(ss.str());                                  \
+    }
+
+#endif  // NDEBUG
+
+/**
+ * Time measurement namespace
+ */
+namespace TM {
+
+using TMClock      = std::chrono::system_clock;
+using TMTimePoint  = std::chrono::time_point<std::chrono::system_clock>;
+using TMString     = std::string;
+using TMTimeStruct = std::tm;
+
+/**
+ * @brief helper functions
+ * @{
+ */
+TMTimePoint GetCurrentTime();
+void        GetLocalTime(const time_t t, TMTimeStruct &tm);
+TMString    CtimeToString(const time_t t);
+void        PrintExecutionResult(const TMString &name, const TMTimePoint start_time);
+
+/**
+ * @brief add a thread id to support multi-thread programs
+ * @param original_name
+ */
+TMString GetComplexName(TMString &&original_name);
+/**
+ *@}
+ */
+
+/**
+ * @brief used to measure block execution time
+ *
+ * put an object of this class in the block, it will measure the execution time from
+ * that point to the block ends
+ *
+ * This is a convinent class used in block without call to Start/Stop in GeneralTimeMeasurement
+ *
+ * @code{.cpp}
+ *
+ * void func() {
+ *    BlockMeasurement measure(__FUNCTION__); // start
+ *    // code been measured
+ *    // measurement end, measurement result will be printed to std::cout
+ * }
+ *
+ * @endcode
+ *
+ */
+class BlockMeasurement {
+ public:
+  BlockMeasurement(std::string &&name);
+  ~BlockMeasurement();
+
+ private:
+  TMString       name_;  // name, used in output
+  TMTimePoint    start_;
+};
+
+/**
+ * @brief general purpose time measurement
+ *
+ * This is a singleton class, you can start from anywhere with call
+ * to Start() and stop anywhere with call to Stop().
+ *
+ * @note name parameter in Start and Stop must be same to complete one measurement
+ *
+ * @code{.cpp}
+ *
+ * void func1() {
+ *    GeneralTimeMeasurement::Instance().Start("test_measurement");
+  *   // code been measured
+ * }
+ *
+ * void func2() {
+ *    GeneralTimeMeasurement::Instance().Stop("test_measurement");
+  *  // code been measured
+ * }
+ *
+
+ *
+ * int main() {
+ *    // measuremtn execution time from fun1 to fun2
+ *    fun1();
+ *    fun2();
+ * }
+ *
+ * @endcode
+ */
+class GeneralTimeMeasurement {
+ public:
+  static GeneralTimeMeasurement &Instance();
+
+  /**
+   * @brief Call start to start measure, call stop to stop measure.
+   * \p name in Start and Stop should be exactly the same, it is the identifier.
+   * @{
+   */
+  void Start(std::string &&name);
+  void Stop(std::string &&name);
+  /**
+   *@}
+   */
+
+ private:
+  GeneralTimeMeasurement() = default;
+
+ private:
+  // name <-> start time
+  std::unordered_map<TMString, TMTimePoint> measurement_map_;
+};
+}  // namespace TM
